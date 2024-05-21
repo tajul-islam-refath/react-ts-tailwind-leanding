7b7e49f6068fef83ba28068db020ad8b232fa749
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-05-21 08:20:57 +0600

    feat: supper quality section design

--- src/sections/SupperQuality.tsx
+++ src/sections/SupperQuality.tsx
@@ -1,5 +1,40 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SupperQuality = () => {
-  return <div>SupperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      <div className="flex flex-1 flex-col">
+        <h2 className=" text-4xl font-palanquin capitalize font-bold lg:max-w-lg">
+          We Provide You
+          <span className="text-coral-red"> Supper</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life
+        </p>
+        <div className="mt-6">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="Shoes"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SupperQuality;
