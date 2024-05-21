e30c6b4103808317a314d5c010cd5a1e5783b90b
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-05-21 08:57:34 +0600

    feat: design Special Offer section design

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,25 @@
 interface ButtonPropos {
   label: string;
   iconUrl?: string;
+  backGroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
 }
 
-const Button = ({ label, iconUrl }: ButtonPropos) => {
+const Button = ({
+  label,
+  iconUrl,
+  backGroundColor,
+  textColor,
+  borderColor,
+}: ButtonPropos) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full ${
+        backGroundColor
+          ? `${backGroundColor} ${textColor} ${borderColor}`
+          : `bg-coral-red  text-white border-coral-red`
+      }`}>
       {label}
       {iconUrl && (
         <img src={iconUrl} alt="icon" className="ml-2 rounded-full w-5 h-5" />
--- src/sections/SpecialOffer.tsx
+++ src/sections/SpecialOffer.tsx
@@ -1,5 +1,44 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
 const SpecialOffer = () => {
-  return <div>SpeacialOffers</div>;
+  return (
+    <section className="flex justify-between items-center max-lg:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="offer"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className=" text-4xl font-palanquin capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red"> Special</span> Offer
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Discover stylish Nike arrivals, quality comfort and innovation for
+          your active life
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+          <Button label="Shop Now" iconUrl={arrowRight} />
+          <Button
+            label="Learn Now"
+            iconUrl={arrowRight}
+            backGroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
